7acfe7ebb49f85c6226ef478822810ec097c5e80
gxlzxv <leonardo.garciav@uanl.edu.mx>
2026-05-15 01:58:46 -0600

    correccion de valor en la tabla doctor

--- doctores/models.py
+++ doctores/models.py
@@ -5,7 +5,7 @@ class Doctor(models.Model):
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     especialidad = models.CharField(max_length=100)
-    telefono = models.IntegerField(max_length=10)
+    telefono = models.CharField(max_length=10)
     
     def __str__(self):
         return f"Dr. {self.apellido} - {self.especialidad}"
