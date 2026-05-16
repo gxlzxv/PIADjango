a45ec2e320f3dc111fe833b496e77220ef22b56d
XimenaIriarte <ximenaa.iriarte015@gmail.com>
2026-05-16 15:38:44 -0600

    Se agregaron templates y archivos static de doctores

--- doctores/views.py
+++ doctores/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def docindex (request):
-    return render(request, 'docindex.html')
+    return render(request, 'doctores/docindex.html')
