a0083cf897b556b135c8275313b4ed33353cb932
XimenaIriarte <ximenaa.iriarte015@gmail.com>
2026-05-16 20:17:22 -0600

    Se agregaron templates y archivos static de home

--- clinicaveterinaria/urls.py
+++ clinicaveterinaria/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("",include('home.urls')),
+    path('home/', include('home.urls')),
     path("doctores/",include('doctores.urls')),
     path("empleados/",include('empleados.urls')),
     path("productos/",include('productos.urls')),
